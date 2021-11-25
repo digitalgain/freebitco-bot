f075b9251b117da46d952487bfae30b958594828
DigitalGain <admin@digitalgain.net>
2021-11-25 08:13:23 +0100

    First commit FreeBitco-bot

---
+++ README.md
@@ -0,0 +1,23 @@
+# FREEBITCO-bot
+The **FREEBITCO-bot** is a free script that automates rolls on the [FREEBITCO.IN](https://freebitco.in/?r=37400527) website; to increase your earnings visit [FREEBITCO-bot informative page](https://digitalgain.net/freebitco-bot.php).
+#### PRE-REQUISITES
+The **FREEBITCO-bot** runs on a NodeJS environment, you must install [**NodeJS**](https://nodejs.org/en/download/) to run the script. Open a terminal and run the following commands to ensure your environment is ready:
+*   `node -v` must return the NodeJS version (version 11+ required)
+*   `npm -v` must return the NPM version (version 6+ required)
+#### INSTALL THE SCRIPT
+*   Clone the repository to your computer
+*   Open a terminal and navigate to the freebitco-bot folder
+*   Launch the command `npm install` to install the script's dependencies
+#### LAUNCH THE SCRIPT
+Before launch the script you must to complete the following steps:
+*   Login [FreeBitco.in website](https://freebitco.in/?r=37400527)
+*   Disable 2FA protection (if this option is enabled, the script can't work)
+*   Ensure you have at least 2500 FUN Token to disable captcha when roll numbers
+
+Then you are ready to launch the script
+*   Launch the command `node freebitco-bot.js -u your_username -p your_password` to run the script
+*   `your_username` is your [FreeBitco.in](https://freebitco.in/?r=37400527) username/email
+*   `your_password` is your [FreeBitco.in](https://freebitco.in/?r=37400527) password
+**The script don't save or send your username and password to third-party.**
+#### DISCLAIMER
+Using this free script you take full responsibility for its operation on your [FreeBitco.in](https://freebitco.in/?r=37400527) account
---
+++ freebitco-bot.js
@@ -0,0 +1,82 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const puppeteer = require("puppeteer");
+const moment = require("moment");
+const process = require("process");
+const minimist = require("minimist");
+const defaultTimeout = 180000;
+const logInfo = (message) => {
+    const now = moment().format();
+    message = (`${now} | ${message}`);
+    console.log(message);
+};
+const launch = (auth) => __awaiter(void 0, void 0, void 0, function* () {
+    const browser = yield puppeteer.launch({
+        headless: auth.headless,
+        defaultViewport: null,
+        devtools: false
+    });
+    const balance = yield roll(browser, auth);
+    yield browser.close();
+    logInfo('Waiting next roll');
+    yield new Promise(resolve => setTimeout(resolve, 3600000));
+    launch(auth);
+});
+const roll = (browser, auth) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const page = yield browser.newPage();
+        page.setDefaultTimeout(defaultTimeout);
+        yield page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36');
+        logInfo('Loading FreeBitco.in');
+        yield page.goto('https://freebitco.in', { waitUntil: 'networkidle0' });
+        logInfo('Logging in');
+        yield page.click('li.login_menu_button > a');
+        yield page.waitForSelector('#login_form', { visible: true });
+        yield page.type('#login_form_btc_address', auth.username);
+        yield page.type('#login_form_password', auth.password);
+        let navi = page.waitForNavigation({ waitUntil: 'networkidle0' });
+        yield page.$eval('#login_button', element => element.click());
+        yield navi;
+        let balance;
+        if (auth.debug === false) {
+            yield page.waitForSelector('#free_play_form_button', { visible: true });
+            logInfo('Rolling');
+            yield page.$eval('#free_play_form_button', el => el.click());
+            yield page.waitForSelector('#free_play_result', { visible: true });
+            const result = yield page.$eval('#free_play_result', el => el.textContent);
+            logInfo(String(result).trim());
+            balance = yield page.$eval('#balance_small', el => el.textContent);
+        }
+        logInfo('Balance: ' + balance);
+        logInfo('Logging out');
+        navi = page.waitForNavigation({ waitUntil: 'networkidle0' });
+        yield page.click('a.logout_link');
+        yield navi;
+        yield page.close();
+        return Number(balance);
+    }
+    catch (err) {
+        logInfo(err);
+        return yield roll(browser, auth);
+    }
+});
+const min = minimist(process.argv);
+if (min.p === undefined || min.u === undefined)
+    throw new Error("Undefined username or password");
+console.log('FREEBITCO-bot');
+console.log('>> https://digitalgain.net/freebitco-bot.php');
+launch({
+    username: min.u,
+    password: min.p,
+    headless: min.h === undefined,
+    debug: min.d !== undefined
+});
